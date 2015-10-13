0d6394a7b3d0b782fa6f7fa5fe2b28d13e7c49d4
GillesVandewiele <givdwiel.vandewiele@ugent.be>
2015-10-13 16:54:27 +0200

    fix for dates?

--- js/controllers/headacheController.js
+++ js/controllers/headacheController.js
@@ -6,8 +6,27 @@
     This file contains the controller to add and modify headaches.
  */
 
+
 var app = angular.module('Chronic', ['onsen.directives']); 
 
 app.controller("headacheController", function($scope){
   $scope.intensityValue=5; // The intensity of the headache
+});
+
+var options = {
+  date: new Date(),
+  mode: 'date'
+};
+
+window.onload = function(){
+	console.log("test");
+};
+
+$("#test").click(function(){
+	console.log("test");
+    // show picker
+    datePicker.show(options, function(date){
+    //get result
+        alert("date result " + date);  
+    });
 });
