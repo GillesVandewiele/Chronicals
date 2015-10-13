12b90eeb8808a1859b6ce11bae6cfbd37dff6d9b
GillesVandewiele <givdwiel.vandewiele@ugent.be>
2015-10-13 16:56:26 +0200

    woohoo fixed

--- js/controllers/headacheController.js
+++ js/controllers/headacheController.js
@@ -12,21 +12,3 @@ var app = angular.module('Chronic', ['onsen.directives']);
 app.controller("headacheController", function($scope){
   $scope.intensityValue=5; // The intensity of the headache
 });
-
-var options = {
-  date: new Date(),
-  mode: 'date'
-};
-
-window.onload = function(){
-	console.log("test");
-};
-
-$("#test").click(function(){
-	console.log("test");
-    // show picker
-    datePicker.show(options, function(date){
-    //get result
-        alert("date result " + date);  
-    });
-});
