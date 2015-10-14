cb7136da85d3e3d9ef436b0da9cbeb2f3e547c95
GillesVandewiele <givdwiel.vandewiele@ugent.be>
2015-10-14 15:57:24 +0200

    trying something to make range work on most androids

--- js/controllers/headacheController.js
+++ js/controllers/headacheController.js
@@ -11,7 +11,23 @@ var app = angular.module('Chronic', ['onsen.directives']);
 
 app.controller("headacheController", function($scope){
   $scope.intensityValue=5; // The intensity of the headache
-  $scope.startDate = new Date();
-  $scope.startTime = new Date();
-  $scope.startTime.setSeconds(0, 0);
+  $scope.startDate = new Date(); // Start date of the headache
+  $scope.startTime = $scope.startDate; // Start time of the headache
+  $scope.symptoms = ["symptom1", "symptom2", "symptom3", "symptom1", "symptom2", "symptom1", "symptom2", "symptom1", "symptom2",
+  "symptom1", "symptom2", "symptom1", "symptom2", "symptom1", "symptom2", "symptom1", "symptom2"]; // List of all symptoms
+  $scope.triggers = ["trigger1", "trigger2", "trigger3"]; // List of all triggers
+  
+  $scope.startTime.setSeconds(0, 0); // We don't care about seconds and milliseconds
+});
+
+$(".timePicker").on("load",function(){
+	console.log("test");
+  $(this).attr("type","text");
+});
+$(".timePicker").on("change",function(){
+	console.log("test");
+  $(this).attr("type","text");
+});
+$(".timePicker").on("touchstart",function(){
+  $(this).attr("type","time");
 });
