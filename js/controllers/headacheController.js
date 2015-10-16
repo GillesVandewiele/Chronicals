5bd5c0ffa953093bf371a2dead2c05d820516666
GillesVandewiele <givdwiel.vandewiele@ugent.be>
2015-10-16 17:04:59 +0200

    added next/finish button + popovers at the help buttons

--- js/controllers/headacheController.js
+++ js/controllers/headacheController.js
@@ -10,21 +10,43 @@
 var app = angular.module('Chronic', ['onsen.directives']); 
 
 app.controller("headacheController", function($scope){
+
+  // Variables & mockup data
   $scope.intensityValue=5; // The intensity of the headache
   $scope.startDate = new Date(); // Start date of the headache
   $scope.startTime = $scope.startDate; // Start time of the headache
-  $scope.symptoms = [{name:"symptom1", description:"this is a description"}, {name:"symptom2", description:"this is a description"},
-  {name:"symptom3", description:"this is a description"}, {name:"symptom4", description:"this is a description"}]; // List of all symptoms
-  $scope.triggers = [{name:"trigger1", description:"this is a description"}, {name:"trigger2", description:"this is a description"},
-  {name:"trigger3", description:"this is a description"}, {name:"trigger4", description:"this is a description"}]; // List of all triggers
+  var months = ["jan.", "feb.", "mrt.", "apr.", "mei", "jun.", "jul.", "aug.", "sept.", "okt.", "nov.", "dec."];
+  $scope.startTimeString = $scope.startTime.getDate() + " " + months[$scope.startTime.getMonth()] + " " + $scope.startTime.getHours() + ":" + $scope.startTime.getMinutes();
+  $scope.symptoms = [{id: 0, name:"symptom1", description:"this is a description"}, {id: 1, name:"symptom2", description:"this is a description"},
+  {id: 2, name:"symptom3", description:"this is a description"}, {id: 3, name:"symptom4", description:"this is a description"}]; // List of all symptoms
+  $scope.triggers = [{id: 0, name:"trigger1", description:"this is a description 1"}, {id: 1, name:"trigger2", description:"this is a description 2"},
+  {id: 2, name:"trigger3", description:"this is a description 3"}, {id: 3, name:"trigger4", description:"this is a description 4"}]; // List of all triggers
+  $scope.message = "";
+
 
   ons.createPopover('popover.html').then(function(popover) {
+  	// Create a popover for the help buttons
     $scope.popover = popover;
   });
   
-  $scope.show = function(e) {
-  	console.log(e);
-    $scope.popover.show(e);
+  var searchIndexById = function(list, id){
+  	// Search the index of an id in a list of objects with ids
+  	for(object in list){
+  		console.log(list[object]);
+  		if(list[object].id == id) return object;
+  	}
+  	return -1;
+  };
+
+  for(trigger in $scope.triggers){
+  	// Initialize function on each helpButton for each trigger
+  	console.log($("#helpButton"+$scope.triggers[trigger].name));
+  	$(document).on("click", '#helpButton'+$scope.triggers[trigger].id, function(){
+  		var id = ($(this)[0].id).split('helpButton');
+  		var index = searchIndexById($scope.triggers, id[1]);
+  		$scope.message = $scope.triggers[index].description;
+  		$scope.popover.show("#"+$(this)[0].id);
+  	});
   };
 
 });
