e4bdbf2c9ab2fa299d67f007d8f4a029fc9261f9
GillesVandewiele <givdwiel.vandewiele@ugent.be>
2015-10-17 14:37:57 +0200

    now with chronic.js

---
+++ js/controllers/chronic.js
@@ -0,0 +1,10 @@
+/*!
+ 	NAAM VAN ONS PROJECT, v1.0
+ 	Created by Kiani Lannoye & Gilles Vandewiele, commissioned by UZ Ghent
+    https://github.com/kianilannoye/Chronicals
+    
+    This file contains the main objects.
+ */
+
+
+var app = angular.module('Chronic', ['onsen.directives']); 
---
+++ js/controllers/medicineController.js
@@ -0,0 +1,11 @@
+/*!
+ 	NAAM VAN ONS PROJECT, v1.0
+ 	Created by Kiani Lannoye & Gilles Vandewiele, commissioned by UZ Ghent
+    https://github.com/kianilannoye/Chronicals
+    
+    This file contains the controller to add and modify medicines.
+ */
+
+angular.module('Chronic').controller("medicineController", function($scope){
+
+});
