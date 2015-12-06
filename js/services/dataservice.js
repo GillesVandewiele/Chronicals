a1b1a2fb347230a9b1807b757320f65fe4b72e61
kianilannoye <kiani_lannoye@hotmail.com>
2015-12-06 13:23:22 +0100

    gitignore fixed

--- js/services/dataservice.js
+++ js/services/dataservice.js
@@ -1,4 +1,16 @@
-angular.module('Chronic').service('dataService', function ($http) {
+angular.module('Chronic').config(['$httpProvider', function ($httpProvider) {
+    $httpProvider.defaults.useXDomain = true;
+    delete $httpProvider.defaults.headers.common["X-Requested-With"];
+    $httpProvider.defaults.headers.common["Accept"] = "application/json";
+
+    $httpProvider.defaults.headers.common["Content-Type"] = "application/json";
+    $httpProvider.defaults.headers.common["Access-Control-Allow-Origin"] = "*";
+    $httpProvider.defaults.headers.common = {};
+    $httpProvider.defaults.headers.post = {};
+    $httpProvider.defaults.headers.put = {};
+    $httpProvider.defaults.headers.patch = {};
+
+}]).service('dataService', function ($http) {
 
     // Reset the local storage; always comment this out!
     //  $localStorage.$reset();
