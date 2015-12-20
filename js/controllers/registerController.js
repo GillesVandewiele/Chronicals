ac809387d243b33834486dc95d23a622530b231a
kianilannoye <kiani_lannoye@hotmail.com>
2015-12-20 13:17:26 +0100

    jajajaj

--- js/controllers/registerController.js
+++ js/controllers/registerController.js
@@ -88,7 +88,7 @@ angular.module('Chronic').config(['$httpProvider', function ($httpProvider) {
             //console.log(data);
             dataService.clearCache();
             dataService.registerUser(data.firstName, data.lastName, data.birthDate, data.isMale, data.relation, data.isEmployed, data.email, data.password, data.patientID);
-            alert("Voor beveiligingsredenen is het nodig om enkele gegevens door te sturen naar de dokters van het uz, zodat ze later uw identiteit aan de data kunnen koppelen. Gelieve in het volgende scherm bij het mailtje op versturen te klikken.");
+            navigator.notification.alert("Voor beveiligingsredenen is het nodig om enkele gegevens door te sturen naar de dokters van het uz, zodat ze later uw identiteit aan de data kunnen koppelen. Gelieve in het volgende scherm bij het mailtje op versturen te klikken.");
             cordova.plugins.email.open({
                     to:          ["uzgent.chronic@gmail.com"], // email addresses for TO field
                     cc:          [], // email addresses for CC field
